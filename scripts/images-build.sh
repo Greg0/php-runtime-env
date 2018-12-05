3d8bf74aadd98c96a159ab040bff036a5a44d714
Grzegorz K <grego_92@o2.pl>
2018-12-05 14:52:01 +0100

    Delete images-build.sh

--- scripts/images-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-docker build -t greg0-nginx $SCRIPTPATH/../images/nginx --force-rm
-docker build -t greg0-php $SCRIPTPATH/../images/php --force-rm
-docker build -t greg0-php-dev $SCRIPTPATH/../images/php-dev --force-rm
